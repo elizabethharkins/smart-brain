02f198efe1850edbef0a9bdae48469268f43825f
Bets <elizabeth.lidstrom@gmail.com>
2019-07-23 19:51:11 -0400

    logo update nav component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '15px' }}>
+			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+		</nav>
+	);
+}
+
+export default Navigation;
