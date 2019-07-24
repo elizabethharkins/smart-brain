8e8cefb585694b5991a61ad9a1b966e7ff8853ae
Bets <elizabeth.lidstrom@gmail.com>
2019-07-24 14:04:09 -0400

    established state re input for form component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
@@ -18,20 +18,36 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles
-        className='particles'
-        params={ particlesOptions }
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-          {/*}  <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  // need to create state here to align app + user
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  // event listener on page, receive 'event'
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          className='particles'
+          params={ particlesOptions }
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+
+        <ImageLinkForm onInputChange={ this.onInputChange } /> 
+            {/*}  <FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
