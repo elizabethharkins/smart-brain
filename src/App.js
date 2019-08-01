7aeab69221122b27450ddb0e2a0574af630dbcd6
Bets <elizabeth.lidstrom@gmail.com>
2019-08-01 19:38:03 -0400

    style update, code clean up

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const app = new Clarifai.App({
 const particlesOptions = {
   particles: {
     number: {
-      value: 100,
+      value: 128,
       density: {
         enable: true,
         value_area: 800
