7f5fd1dbb6f8b2d82947b5419c3c178a91ffb714
Bets <elizabeth.lidstrom@gmail.com>
2019-08-02 13:53:24 -0400

    app routing

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://blooming-beach-32576.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://blooming-beach-32576.herokuapp.com:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
