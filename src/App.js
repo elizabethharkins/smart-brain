7d2e5c9deb6ae5b9e093dc612327416cca5445d3
Bets <elizabeth.lidstrom@gmail.com>
2019-08-02 14:40:44 -0400

    port fix

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://blooming-beach-32576.herokuapp.com:3000/image', {
+          fetch('https://blooming-beach-32576.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
