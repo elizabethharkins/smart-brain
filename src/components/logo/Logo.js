9ac262e53005db541fd3f6c53a6639852437eda2
Bets <elizabeth.lidstrom@gmail.com>
2019-08-14 20:19:28 -0400

    updated logo and form for style

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 
 const Logo = () => {
 	return (
-		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }}>
+		<div style={{ position: 'absolute', top: '0', padding: '20px' }}>
+			<Tilt className="Tilt br2 shadow-2" options={{ max: '55' }} style={{ height: '130', width: '160' }}>
 				<div className="Tilt-inner pa3">
-					<img style={{ paddingTop: '25px' }} alt='logo' src={ brain }/>
+					<img style={{ padding: '15px' }} alt='logo' src={ brain }/>
 				</div>
 			</Tilt>
 		</div>
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
 	return (
 		<div>
-			<div className='white f3'>
+			<div className='white f3 pa3' style={{ marginTop: '75px' }}>
 				{ `${ name }, your current entry count is ...` }
 			</div>
 			<div className='white f1'>
